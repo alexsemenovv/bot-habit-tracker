3b0dd90e3f7167a3a7e94fa3e777fe849e21ce60
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-04-13 13:49:32 +0500

    added request_to_new_habit

--- bot/handlers/custom_handlers/new_habit.py
+++ bot/handlers/custom_handlers/new_habit.py
@@ -3,7 +3,9 @@ from datetime import datetime
 from telebot.types import Message
 
 from loader import bot
+from request_to_api.users_api import get_user_by_tg_id
 from states.habit_info import HabitInfoState
+from request_to_api.habits_api import request_to_new_habit
 
 
 @bot.message_handler(commands=['new_habit'])
@@ -29,7 +31,7 @@ def get_name(message: Message) -> None:
 @bot.message_handler(state=HabitInfoState.description)
 def get_description(message: Message) -> None:
     """Получение описания привычки"""
-    bot.send_message(message.from_user.id, 'Описание добавлено. Введите дату начала выполнения привычки, в формате DD-MM-YYYY: ')
+    bot.send_message(message.from_user.id, 'Описание добавлено. Введите дату начала выполнения привычки, в формате YYYY-MM-DD: ')
     bot.set_state(message.from_user.id, HabitInfoState.start_date, message.chat.id)  # меняем состояние
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:  # добавляем в класс состояния - описание
@@ -39,12 +41,16 @@ def get_description(message: Message) -> None:
 @bot.message_handler(state=HabitInfoState.start_date)
 def get_start_date(message: Message) -> None:
     """Получение даты начала выполнения привычки"""
-    start = datetime.strptime(message.text, "%d-%m-%Y")
+    start = datetime.strptime(message.text, "%Y-%m-%d").isoformat()
     bot.send_message(message.from_user.id, 'Дата начала записана!')
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:  # добавляем в класс состояния - дату начала
         data['start_date'] = start
-
-    text = f"Привычка создана!\nНазвание привычки: {data['name']}\nОписание: {data['description']}\nДата начала: {message.text}"
+        data['user_id'] = get_user_by_tg_id(message.from_user.id).get('id')
+    result = request_to_new_habit(data)
+    if result:
+        text = f"Привычка создана!\nНазвание привычки: {data['name']}\nОписание: {data['description']}\nДата начала: {message.text}"
+    else:
+        text = "Ошибка"
     bot.send_message(message.from_user.id, text)
 
---
+++ bot/request_to_api/habits_api.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+import requests
+
+API_URL = "http://127.0.0.1:8000/api/habits"  # Адрес FastAPI
+
+def request_to_new_habit(habit: Dict) -> bool:
+    """Отправка запроса на добавление новой привычке"""
+    response = requests.post(API_URL, json=habit)
+    if response.status_code == 200:
+        return True
+    return False
