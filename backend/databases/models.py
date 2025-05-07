276c7ba3c9a86f34fed5761443f09aacfd99a3c3
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-05-07 16:27:27 +0500

    added HabitTrack; added target_days in Habit

--- backend/databases/models.py
+++ backend/databases/models.py
@@ -26,7 +26,6 @@ class User(Base):
     is_bot = Column(Boolean, default=False, nullable=False)
 
     habits = relationship("Habit", back_populates="user")
-    duration_of_habits = relationship("DurationOfHabits", back_populates="habit")
 
     def __str__(self) -> str:
         return f"Пользователь {self.first_name} {self.username}"
@@ -41,20 +40,26 @@ class Habit(Base):
     description = Column(Text, nullable=True)
     start_date = Column(Date, nullable=False, default=dt.date(dt.now()))
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    target_days = Column(Integer, nullable=False, default=21)
     is_active = Column(Boolean, nullable=True, default=True, index=True)
 
     user = relationship("User", back_populates="habits")
-    duration_of_habits = relationship("DurationOfHabits", back_populates="habit")
+    habit_track = relationship("HabitTrack", back_populates="habit")
 
     def __str__(self) -> str:
         return f"Привычка: {self.name}\nДата начала: {dt.strftime(self.start_date, '%d-%m-%Y')}"
 
 
-class DurationOfHabits:
-    """Продолжительность выполнения привычки"""
-    __tablename__ = "duration_of_habits"
+class HabitTrack(Base):
+    """Отслеживает невыполнение/выполнение привычки в этот день"""
+    __tablename__ = "habit_track"
     id = Column(Integer, primary_key=True, index=True)
     habit_id = Column(Integer, ForeignKey("habits.id"), nullable=False)
     date_of_completion = Column(Date, nullable=False, default=dt.date(dt.now()))
+    is_done = Column(Boolean, nullable=False, index=True)
 
-    habit = relationship("Habit", back_populates="duration_of_habits")
+    habit = relationship("Habit", back_populates="habit_track")
+
+    def __str__(self) -> str:
+        return (f"id привычки: {self.habit_id}\nДата выполнения: {dt.strftime(self.date_of_completion, '%d-%m-%Y')}"
+                f"\nПривычка в этот день выполнена? {'Да' if self.is_done else 'Нет'}")
