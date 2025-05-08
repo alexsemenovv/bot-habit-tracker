4e7bcdd4c308e5830a5e3187a9dc768ce1f8705f
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-05-08 12:11:07 +0500

    added InlineKeyboardMarkup for 'show_list_habits'

--- bot/handlers/custom_handlers/list_habits.py
+++ bot/handlers/custom_handlers/list_habits.py
@@ -1,20 +1,31 @@
+from typing import List
+
 from loader import bot
 from request_to_api.habits_api import request_to_get_all_active_habits
-from telebot.types import Message
+from telebot.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def gen_inline_markup(buttons: List[dict]) -> InlineKeyboardMarkup:
+    """
+    Создание Inline клавиатуры
+    :param buttons: List  - список названий кнопок
+    :return: клавиатуру InlineKeyboardMarkup
+    """
+    buttons = [InlineKeyboardButton(text=i_btn["name"], callback_data=i_btn["name"]) for i_btn in buttons]
+    keyboard =  InlineKeyboardMarkup()
+    keyboard.add(*buttons)
+    return keyboard
+
 
 
 @bot.message_handler(commands=["list_habits"])
 def show_list_habits(message: Message) -> None:
     """Просмотр списка всех действующих привычек"""
-    bot.send_message(message.from_user.id, "Ваши действующие привычки: ")
     response = request_to_get_all_active_habits()
     if response:
-        for i_habit in response:
-            info = "{id}) *Название:* {name}\n*Описание:* {description}\n*Дата начала:* {start}".format(
-                id=i_habit.get("id"),
-                name=i_habit.get("name"),
-                description=i_habit.get("description"),
-                start=i_habit.get("start_date"),
-            )
+        bot.send_message(message.from_user.id, "Ваши действующие привычки: ", reply_markup=gen_inline_markup(response))
+    else:
+        bot.send_message(message.from_user.id, "*Вы еще не добавили ни одной привычки*", parse_mode="Markdown")
 
-            bot.send_message(message.from_user.id, info, parse_mode="Markdown")
+
+
