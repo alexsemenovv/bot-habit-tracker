e689d40691f9a3286bb3d26f861a005afcde9590
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-05-08 14:29:47 +0500

    added ReplyKeyBoardMarkup for actions with habit

--- bot/handlers/custom_handlers/list_habits.py
+++ bot/handlers/custom_handlers/list_habits.py
@@ -2,7 +2,13 @@ from typing import List
 
 from loader import bot
 from request_to_api.habits_api import request_to_get_all_active_habits
-from telebot.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from telebot.types import (
+    Message,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
 
 
 def gen_inline_markup(buttons: List[dict]) -> InlineKeyboardMarkup:
@@ -11,11 +17,25 @@ def gen_inline_markup(buttons: List[dict]) -> InlineKeyboardMarkup:
     :param buttons: List  - список названий кнопок
     :return: клавиатуру InlineKeyboardMarkup
     """
-    buttons = [InlineKeyboardButton(text=i_btn["name"], callback_data=i_btn["name"]) for i_btn in buttons]
-    keyboard =  InlineKeyboardMarkup()
+    buttons = [InlineKeyboardButton(text=i_btn["name"], callback_data='habit_' + str(i_btn["id"])) for i_btn in buttons]
+    keyboard = InlineKeyboardMarkup()
     keyboard.add(*buttons)
     return keyboard
 
+
+def gen_reply_markup(buttons: List[str]) -> ReplyKeyboardMarkup:
+    """
+    Создание Reply клавиатуры
+    :param buttons: List  - список названий кнопок
+    :return: клавиатуру ReplyKeyboardMarkup
+    """
+    buttons = [
+        KeyboardButton(text=btn)
+        for btn in buttons
+    ]
+    keyboard = ReplyKeyboardMarkup()
+    keyboard.add(*buttons)
+    return keyboard
 
 
 @bot.message_handler(commands=["list_habits"])
@@ -28,4 +48,8 @@ def show_list_habits(message: Message) -> None:
         bot.send_message(message.from_user.id, "*Вы еще не добавили ни одной привычки*", parse_mode="Markdown")
 
 
-
+@bot.callback_query_handler(func=lambda callback_query: (callback_query.data.startswith("habit_")))
+def handle_habit_selection(callback_query):
+    """Обработчик, предоставляет Reply клавиатуру, для выбора действия с привычкой"""
+    actions = ["Описание", "Редактировать", "Удалить", "Отметить выполненной", "🔙Назад"]
+    bot.send_message(callback_query.from_user.id, "Выберите действие: ", reply_markup=gen_reply_markup(actions))
