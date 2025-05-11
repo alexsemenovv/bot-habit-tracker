946399ca3ab77626c81980e7c64c513896205ec9
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-05-11 14:19:52 +0500

    added btn 'back'

--- bot/handlers/custom_handlers/actions.py
+++ bot/handlers/custom_handlers/actions.py
@@ -1,7 +1,8 @@
+from handlers.custom_handlers.list_habits import gen_inline_markup
 from loader import bot
 from request_to_api.habits_api import (
     request_to_get_habit_by_id,
-    request_to_delete_habit_by_id,
+    request_to_delete_habit_by_id, request_to_get_all_active_habits,
 )
 from telebot.types import CallbackQuery
 
@@ -13,7 +14,7 @@ def handle_description_habit(callback_query: CallbackQuery) -> None:
     """
     Обработчик, предоставляет описание привычки
     :param callback_query: CallbackQuery - запрос, который начинается на 'description_'
-    :return: описание привычки
+    :return: None
     """
     habit_id = int(callback_query.data.split("_")[1])
     habit = request_to_get_habit_by_id(habit_id)
@@ -41,11 +42,35 @@ def handle_delete_habit(callback_query: CallbackQuery) -> None:
     """
     Обработчик, удаляет привычку
     :param callback_query: CallbackQuery - запрос, который начинается на 'delete_'
-    :return: True - если удалена, иначе False
+    :return: None
     """
     habit_id = int(callback_query.data.split("_")[1])
     result = request_to_delete_habit_by_id(habit_id)
     if result:
-        bot.send_message(callback_query.from_user.id, "Привычка удалена!")
+        bot.send_message(callback_query.from_user.id, "Привычка удалена!\nК списку привычек /list_habits")
     else:
         bot.send_message(callback_query.from_user.id, 'Ошибка запроса')
+
+
+@bot.callback_query_handler(
+    func=lambda callback_query: (callback_query.data.startswith("back"))
+)
+def handle_btn_back(callback_query: CallbackQuery) -> None:
+    """
+    При нажатии на кнопку 'назад' - возвращает список привычек
+    :param callback_query: CallbackQuery - запрос, который начинается на 'back'
+    :return: None
+    """
+    response = request_to_get_all_active_habits()
+    if response:
+        bot.send_message(
+            callback_query.from_user.id,
+            "Ваши действующие привычки: ",
+            reply_markup=gen_inline_markup(response),
+        )
+    else:
+        bot.send_message(
+            callback_query.from_user.id,
+            "*Вы еще не добавили ни одной привычки*",
+            parse_mode="Markdown",
+        )
